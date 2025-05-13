5e1034219cff88acd0561286e1a6209a7ef0b82d
Vivek Anand <thevivek9939@gmail.com>
2025-05-13 11:00:09 +0530

    Loading added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{Suspense, useState} from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import PlaceOrder from './Pages/PlaceOrder/PlaceOrder'
@@ -8,6 +8,7 @@ import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LoginPopup/LoginPopup'
 import Verify from './Pages/Verify/Verify'
 import MyOrders from './Pages/MyOrders/MyOrders'
+import Loader from './components/Loader/Loader'
 
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
     {showLogin? <LoginPopup setShowLogin={setShowLogin}/>:<></>}
     <div className='app'>
       <Navbar setShowLogin={setShowLogin} />
+      <Suspense fallback={<Loader/>}>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/Cart' element={<Cart/>}/>
@@ -25,6 +27,7 @@ const App = () => {
         <Route path='/myOrder' element={<MyOrders/>}/>
         
       </Routes>
+      </Suspense>
     </div>
     <Footer/>
     </>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,17 @@
-import React, { useState } from 'react'
+import React, { lazy, Suspense, useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
-import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
+ const FoodDisplay = lazy(()=> import ( '../../components/FoodDisplay/FoodDisplay') )
 import AppDownload from '../../components/AppDownload/AppDownload'
+import Loader from '../../components/Loader/Loader'
 
 const Home = () => {
   const [category,setCategory]=useState('All');
   return (
     <div>
       <Header/>
+      <Suspense fallback={<Loader/>}></Suspense>
       <ExploreMenu category={category} setCategory={setCategory}/>
       <FoodDisplay category={category}/>
       <AppDownload/>
